ba506f8ed753bc556474f1413663be7e89e545eb
c.gadiel <claudiomorera29@gmail.com>
2022-12-29 15:43:58 -0600

    Claudio Morera

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ Use the getLastReview function below to do the following:
 
 
 function getLastReview(array) {
-  return `${array[reviews.length - 1].name} gave the restaurant a ${array[reviews.length - 1].rating} star review, and their feedback was: ${array[reviews.length - 1].feedback}`;
+  return `${array[array.length - 1].name} gave the restaurant a ${array[array.length - 1].rating} star review, and their feedback was: ${array[array.length - 1].feedback}`;
   
 } 
 getLastReview(reviews);
